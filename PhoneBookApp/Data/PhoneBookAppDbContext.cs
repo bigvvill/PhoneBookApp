8b3f327e96badef2557de93264075e2c23e678b7
bigvvill <wcurtisusa@gmail.com>
2022-11-08 00:11:58 -0500

    Working on DbContext

---
+++ PhoneBookApp/Data/PhoneBookAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBookApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApp.Data
+{
+    public class PhoneBookAppDbContext : DbContext
+    {
+        public PhoneBookAppDbContext(DbContextOptions<Contact> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+
+    
+}
--- PhoneBookApp/Models/Contact.cs
+++ PhoneBookApp/Models/Contact.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 
 namespace PhoneBookApp.Models
 {
-    internal class Contact
+    public class Contact
     {
         [Required]
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public string PhoneNumber { get; set; }
     }
 }
